519c420b5c5bd45136b18d88976cea252edd48a4
un-Pun <106893166+un-Pun@users.noreply.github.com>
2023-07-25 02:19:05 +0800

    Update streamlit_app.py
    Added Line 34: To stop streamlit temporary.
    Deleted commented block of line.

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,13 +31,10 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do?
 streamlit.dataframe(fruityvice_normalized)
 
+# Don't run anything past here while we troubleshoot
+streamlit.stop()
+
 import snowflake.connector
-#my_cnx1 = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-#my_cur1 = my_cnx1.cursor()
-#my_cur1.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
-#my_data_row1 = my_cur1.fetchone()
-#streamlit.text("Hello from Snowflake:")
-#streamlit.text(my_data_row1)
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
