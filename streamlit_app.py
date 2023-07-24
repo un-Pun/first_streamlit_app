b838a80145a89fd2563fbd47137093f219d2570a
un-Pun <106893166+un-Pun@users.noreply.github.com>
2023-07-25 01:58:04 +0800

    Update streamlit_app.py
    Added a line that should return 'Banana'.

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,9 +32,16 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
 
 import snowflake.connector
+my_cnx1 = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur1 = my_cnx.cursor()
+my_cur1.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_data_row1 = my_cur1.fetchone()
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row1)
+
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
-my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_cur.execute("SELECT * FROM fruit_load_list")
 my_data_row = my_cur.fetchone()
-streamlit.text("Hello from Snowflake:")
+streamlit.text("This fruit load list contains:")
 streamlit.text(my_data_row)
