edcd301316c47807fe5302d15755e7c066830c48
un-Pun <106893166+un-Pun@users.noreply.github.com>
2023-07-25 00:57:23 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit
 import pandas
 import requests
-import snowflake-connector-python
+import snowflake-connector
 
 streamlit.title ("My Parents New Healthy Dinner");
 streamlit.header ("Breakfast Menu");
