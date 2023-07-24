853589c222fa2656b34103271a2c44d666ecadd4
un-Pun <106893166+un-Pun@users.noreply.github.com>
2023-07-25 02:12:26 +0800

    Update streamlit_app.py
    Added Line 51: Should show a success message.

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,3 +48,4 @@ streamlit.dataframe(my_data_rows)
 
 # Allow the end user to add a fruit to the list
 add_my_fruit = streamlit.text_input('Add a new fruit to the list:')
+streamlit.success(f"Successfully added {add_my_fruit} to the list!")
